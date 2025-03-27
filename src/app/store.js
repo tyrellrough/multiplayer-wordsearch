ef48b14645ece70baed52dbfb372ca24ebfc1297
Tyrell <tyrellrough@outlook.com>
2025-03-27 15:55:32 +0000

    Removed .js from imports

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { wordSearchAPI } from "../services/wordSearchAPI.js";
 
-
-import gameOptionsReducer from './../features/gameOptions/gameOptionsSlice.js';
-import pageSwitcherReducer from './../features/pageSwitcher/pageSwitcherSlice.js';
-import screenReducer from './../features/screen/screenSlice.js';
-import singlePlayerGameReducer from './../features/gameSinglePlayer/singlePlayerGameSlice.js';
-import gameAudioReducer from './../features/audio/gameAudioSlice.js';
-import multiPlayerGameReducer from './../features/gameMultiPlayer/multiPlayerGameSlice.js';
+import gameOptionsReducer from './../features/gameOptions/gameOptionsSlice';
+import pageSwitcherReducer from './../features/pageSwitcher/pageSwitcherSlice';
+import screenReducer from './../features/screen/screenSlice';
+import singlePlayerGameReducer from './../features/gameSinglePlayer/singlePlayerGameSlice';
+import gameAudioReducer from './../features/audio/gameAudioSlice';
+import multiPlayerGameReducer from './../features/gameMultiPlayer/multiPlayerGameSlice';
 
 export const store = configureStore({
     reducer: {
