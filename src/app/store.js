8b5a4af0f9a82d421c2b063e46eb470741ba4dd0
Tyrell <tyrellrough@outlook.com>
2025-03-27 14:44:57 +0000

    Added ./ to imports

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,11 @@ import { wordSearchAPI } from "../services/wordSearchAPI.js";
 
 
 import gameOptionsReducer from "./../features/gameOptions/gameOptionsSlice.js";
-import pageSwitcherReducer from "../features/pageSwitcher/pageSwitcherSlice.js";
-import screenReducer from "../features/screen/screenSlice.js";
-import singlePlayerGameReducer from "../features/gameSinglePlayer/singlePlayerGameSlice.js";
-import multiplayerGameReducer from "../features/gameMultiPlayer/multiPlayerGameSlice.js";
-import gameAudioReducer from "../features/audio/gameAudioSlice.js";
+import pageSwitcherReducer from "./../features/pageSwitcher/pageSwitcherSlice.js";
+import screenReducer from "./../features/screen/screenSlice.js";
+import singlePlayerGameReducer from "./../features/gameSinglePlayer/singlePlayerGameSlice.js";
+import multiplayerGameReducer from "./../features/gameMultiPlayer/multiPlayerGameSlice.js";
+import gameAudioReducer from "./../features/audio/gameAudioSlice.js";
 
 export const store = configureStore({
     reducer: {
