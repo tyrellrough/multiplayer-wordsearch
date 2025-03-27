3824a14de2f8401a7692e1277f8e7a26eef37f89
Tyrell <tyrellrough@outlook.com>
2025-03-27 14:52:34 +0000

    changed some " to '

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import { wordSearchAPI } from "../services/wordSearchAPI.js";
 
 
-import gameOptionsReducer from "./../features/gameOptions/gameOptionsSlice.js";
-import pageSwitcherReducer from "./../features/pageSwitcher/pageSwitcherSlice.js";
-import screenReducer from "./../features/screen/screenSlice.js";
-import singlePlayerGameReducer from "./../features/gameSinglePlayer/singlePlayerGameSlice.js";
-import multiplayerGameReducer from "./../features/gameMultiPlayer/multiPlayerGameSlice.js";
-import gameAudioReducer from "./../features/audio/gameAudioSlice.js";
+import gameOptionsReducer from './../features/gameOptions/gameOptionsSlice.js';
+import pageSwitcherReducer from './../features/pageSwitcher/pageSwitcherSlice.js';
+import screenReducer from './../features/screen/screenSlice.js';
+import singlePlayerGameReducer from './../features/gameSinglePlayer/singlePlayerGameSlice.js';
+import multiplayerGameReducer from './../features/gameMultiPlayer/multiPlayerGameSlice.js';
+import gameAudioReducer from './../features/audio/gameAudioSlice.js';
 
 export const store = configureStore({
     reducer: {
